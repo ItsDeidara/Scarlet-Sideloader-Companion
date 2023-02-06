e8903c8126554a63ce10d803b6a05a89bcafb103
ItsDeidara <r4g3ing@gmail.com>
2023-02-06 16:07:06 -0500

    Added Randomize Name Option
    There is now a button to pick a random 32 character name
    
    The program will also automatically load your partner token from a myPartnerToken.txt file in the same directory as the script!

--- Scarlet Sideloader Companion.ps1
+++ Scarlet Sideloader Companion.ps1
@@ -15,7 +15,19 @@ $wantedAppNameTextBox.Text = "Enter the desired for your app"
 $myPartnerTokenTextBox = New-Object System.Windows.Forms.TextBox
 $myPartnerTokenTextBox.Location = New-Object System.Drawing.Point(10, 40)
 $myPartnerTokenTextBox.Size = New-Object System.Drawing.Size(400, 20)
-$myPartnerTokenTextBox.Text = "Enter your partner token"
+
+# Check if partner token file exists
+# Get the path of the script
+$scriptPath = (Get-Location).Path
+
+# Build the full path to the partner token file
+$myPartnerTokenFile = Join-Path $scriptPath "myPartnerToken.txt"
+
+# Check if partner token file exists
+(Test-Path $myPartnerTokenFile) 
+    # Read partner token from file
+    $myPartnerToken = Get-Content $myPartnerTokenFile | Select-Object -First 1
+    $myPartnerTokenTextBox.Text = $myPartnerToken
 
 # Define Dropdown List
 $appxToUploadComboBox = New-Object System.Windows.Forms.ComboBox
@@ -23,20 +35,27 @@ $appxToUploadComboBox.Location = New-Object System.Drawing.Point(10, 70)
 $appxToUploadComboBox.Size = New-Object System.Drawing.Size(400, 20)
 $appxToUploadComboBox.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList
 
+# Define Button for Randomizing App Name
+$randomizeButton = New-Object System.Windows.Forms.Button
+$randomizeButton.Location = New-Object System.Drawing.Point(10, 100)
+$randomizeButton.Size = New-Object System.Drawing.Size(400, 40)
+$randomizeButton.Text = "Randomize Name"
+
 # Populate Dropdown List with Apps in "appsToUpload" folder
 $appsToUploadPath = (Get-Location).Path + "\appsToUpload"
 Get-ChildItem -Path $appsToUploadPath | Foreach-Object {
     $appxToUploadComboBox.Items.Add($_.Name)
 }
 
-# Define Button
+# Define Upload Button
 $uploadButton = New-Object System.Windows.Forms.Button
-$uploadButton.Location = New-Object System.Drawing.Point(10, 100)
+$uploadButton.Location = New-Object System.Drawing.Point(10, 150)
 $uploadButton.Size = New-Object System.Drawing.Size(400, 40)
 $uploadButton.Text = "Upload"
 
 # Add controls to Form
 $form.Controls.Add($wantedAppNameTextBox)
+$form.Controls.Add($randomizeButton)
 $form.Controls.Add($myPartnerTokenTextBox)
 $form.Controls.Add($appxToUploadComboBox)
 $form.Controls.Add($uploadButton)
@@ -50,6 +69,15 @@ $uploadButton.Add_Click({
     & dotnet Scarlett-Sideloader.dll -n $wantedAppName -p $myPartnerToken $appxToUpload
     Start-Sleep -s 2
     $form.Close()
+})
+
+$randomizeButton.Add_Click({
+    $chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    $wantedAppName = ''
+    for ($i = 0; $i -lt 32; $i++) {
+        $wantedAppName += $chars[(Get-Random -Maximum $chars.Length)]
+    }
+    $wantedAppNameTextBox.Text = $wantedAppName
 })
 
 # Show Form
